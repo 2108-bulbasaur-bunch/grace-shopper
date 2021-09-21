7dfa1dc25ac869737f268474f3af43d4d81967b7
Sarah Sheppard <sarahmariesheppard@gmail.com>
2021-09-21 10:59:34 -0400

    completed product

--- server/db/index.js
+++ server/db/index.js
@@ -3,14 +3,24 @@
 const db = require('./db')
 
 const User = require('./models/User')
-const Product = require('./models/Product');
+const Order = require('./models/Order')
+const Cart = require('./models/Cart')
+const Product = require('.models/Product')
 
 //associations could go here!
+
+User.hasMany(Order);
+Order.belongsTo(User);
+
+Product.belongsToMany(Order, { through: 'Cart' })
+Order.belongsToMany(Product, { through: 'Cart' })
 
 module.exports = {
   db,
   models: {
     User,
-    Product,
+    Order,
+    Cart,
+    Product
   },
 }
