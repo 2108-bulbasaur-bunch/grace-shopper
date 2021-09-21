30c0bb605175b910de9ab41afa6d345048362ab7
Ava Choi <alexbchoi@gmail.com>
2021-09-21 14:57:24 -0400

    usersRoute

--- server/api/users.js
+++ server/api/users.js
@@ -1,17 +1,25 @@
-const router = require('express').Router()
-const { models: { User }} = require('../db')
-module.exports = router
+const router = require("express").Router();
+const {
+	models: { User },
+} = require("../db");
+const { isLoggedIn, isAdmin } = require("./adminFunc");
 
-router.get('/', async (req, res, next) => {
-  try {
-    const users = await User.findAll({
-      // explicitly select only the id and username fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'username']
-    })
-    res.json(users)
-  } catch (err) {
-    next(err)
-  }
-})
+module.exports = router;
+
+//Get: user listing page for admin only
+//+  need to check if it is admin first
+router.get("/", isLoggedIn, isAdmin, async (req, res, next) => {
+	try {
+		const users = await User.findAll({
+			// explicitly select only the id and username fields - even though
+			// users' passwords are encrypted, it won't help if we just
+			// send everything to anyone who asks!
+			attributes: ["id", "firstName", "lastName", "email"],
+		});
+		res.json(users);
+	} catch (err) {
+		next(err);
+	}
+});
+
+//POST: sign-up
