8159a1c44daefbbff732323b4c24019898923305
Ava Choi <alexbchoi@gmail.com>
2021-09-27 14:56:12 -0400

    fixingBugs

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -4,10 +4,33 @@ import { fetchSingleProduct, updateProductThunk } from "../store/oneProduct";
 import {addItemThunk} from '../store/cart'
 
 class SingleProduct extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={quantity:''}
+    this.handleSubmit=this.handleSubmit.bind(this)
+    this.handleChange=this.handleChange.bind(this)
+  }
   componentDidMount() {
     const productId = this.props.match.params.productId;
     this.props.getSingleProduct(productId);
   }
+
+  async handleSubmit(event){
+    console.log('event.target',event.target)
+    console.log('this.state',this.state)
+    event.preventDefault();
+    try {
+      await this.props.addItem(this.state)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    }
 
   render() {
     const {product} = this.props;
@@ -19,8 +42,14 @@ class SingleProduct extends React.Component {
         <h5>{product.price}</h5>
         <p>{product.description}</p>
         <p>Quantity Left: {product.quantity}</p>
-        <form>
+        <form onSubmit={this.handleSubmit}> 
           {/* <label>Quantity: <input type="text"/> </label>*/}
+           <select onChange={this.handleChange}>
+              <option value='1'>1</option>
+               <option value='2'>2</option>
+                <option value='3'>3</option>
+                 <option value='4'>4</option>
+           </select> 
           <button type="submit">Add to Cart</button>
         </form>
       </div>
