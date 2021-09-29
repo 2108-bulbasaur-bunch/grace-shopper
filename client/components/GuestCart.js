dd86a404a3eb273aec78737d2433545322557707
Sarah Sheppard <sarahmariesheppard@gmail.com>
2021-09-29 09:05:01 -0400

    styling carts

--- client/components/GuestCart.js
+++ client/components/GuestCart.js
@@ -6,6 +6,7 @@ class GuestCart extends React.Component {
   constructor(props) {
     super(props);
     this.state = { cart: [] };
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -15,31 +16,84 @@ class GuestCart extends React.Component {
     this.props.getProducts();
   }
 
+  async handleChange(event) {
+    await this.setState({ value: event.target.value });
+  }
+
   render() {
     const cart = this.state.cart;
     const products = this.props.products;
+    const options2 = { style: "currency", currency: "USD" };
 
     let quantity = 0;
     console.log("PRODUCTS", products);
 
     return (
       <div>
+        <h2>Cart</h2>
+        <p>
+          Welcome, Guest! You will need to <a href="/login">login</a> or{" "}
+          <a href="/signup">sign up</a> in order to complete your order.
+        </p>
         {!cart[0] ? (
           <div>Add some sweets!</div>
         ) : (
           <div>
             {cart.map((item, idx) => {
               return (
-                <div key={idx}>
+                <div key={idx} className="item-in-cart">
+                  <div>
+                    {!products[0] ? (
+                      <div>loading</div>
+                    ) : (
+                      <img
+                        src={
+                          products.filter(
+                            (product) => product.id === item.productId
+                          )[0].imageUrl
+                        }
+                        width="150"
+                        height="150"
+                      />
+                    )}
+                  </div>
                   <div>
                     {!products[0] ? (
                       <div>loading</div>
                     ) : (
                       <div>
-                      {products.filter((product)=> product.id === item.productId)[0].name}
-                      <div>
-                      <h4>{item.quantity}</h4>
-                      </div>
+                        {
+                          products.filter(
+                            (product) => product.id === item.productId
+                          )[0].name
+                        }
+                        <div>
+                          <p>Quantity: {item.quantity}</p>
+                        </div>
+                        {/* <div>
+                          Quantity:
+                        <select onChange={this.handleChange}>
+                          {Array.from(Array(item.quantity), (e, i) => {
+                            return (
+                              <option key={i} value={i + 1}>
+                                {i + 1}
+                              </option>
+                            );
+                          })}
+                        </select>
+                        </div> */}
+                        <div className="purchase-price-guest">
+                          Price:{" "}
+                          {new Intl.NumberFormat("en-US", options2).format(
+                            item.purchasePrice / 100
+                          )}
+                        </div>
+                        <button
+                          type="button"
+                          // onClick={this.handleChange(cart.splice(cart.indexOf(item), 1))}
+                        >
+                          Delete
+                        </button>
                       </div>
                     )}
                   </div>
@@ -47,7 +101,7 @@ class GuestCart extends React.Component {
               );
             })}
             <button type="button">
-            <a href="/signup">Checkout</a>
+              <a href="/signup">Checkout</a>
             </button>
           </div>
         )}
