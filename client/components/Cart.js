c0bd3f3e7c525fa69083be72929fddf8db87bcae
Ava Choi <alexbchoi@gmail.com>
2021-09-29 09:08:22 -0400

    finishedId

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -13,6 +13,7 @@ class Cart extends React.Component {
 	constructor(props) {
 		super(props);
 		this.changeQty = this.changeQty.bind(this);
+		this.checkout = this.checkout.bind(this);
 	}
 
 	componentDidMount() {
@@ -20,7 +21,7 @@ class Cart extends React.Component {
 		this.props.getProducts();
 		this.props.getCart(id);
 		let guestCart = JSON.parse(localStorage.getItem("cart"));
-		this.props.addItems(id, guestCart)
+		this.props.addItems(id, guestCart);
 	}
 
 	async changeQty(item, value) {
@@ -29,94 +30,106 @@ class Cart extends React.Component {
 		await this.props.changeQty(this.props.match.params.userId, updatedItem);
 	}
 
+	async checkout(id) {
+		await this.props.checkoutCart(id);
+		localStorage.removeItem("cart");
+	}
+
 	render() {
-		let cart = this.props.cart
+		let cart = this.props.cart;
 		const products = this.props.products;
 		const id = this.props.match.params.userId;
 
-    let quantity = 0;
-    return (
-      <div>
-        <h2>Cart</h2>
-        {!cart[0] ? (
-          <div>empty cart</div>
-        ) : (
-          <div>
-            {cart.map((item, idx) => (
-              <div key={idx} className="item-in-cart">
-                  <div>
-                  {!products[0] ? (
-                    <div>loading</div>
-                  ) : (
-                    <img src={products.filter(
-                      (product) => product.id === item.productId
-                    )[0].imageUrl} width="150" height="150"/>
-                  )}
-                </div>
-                <div>
-                  {!products[0] ? (
-                    <div>loading</div>
-                  ) : (
-                    products.filter(
-                      (product) => product.id === item.productId
-                    )[0].name
-                  )}
-                </div>
-                  <div className="quantity-tracker">in stock:
-                  {
-                    (quantity = products.filter(
-                      (product) => product.id === item.productId
-                    )[0].quantity)
-                  }
-                  </div>
-                  <div>
-                    Quantity:
-                  <select
-                    onChange={(event) =>
-                      this.changeQty(item, event.target.value)
-                    }
-                  >
-                    {Array.from(Array(quantity), (e, i) => {
-                      return (
-                        <option
-                          value={i + 1}
-                          selected={i + 1 == item.quantity ? true : false}
-                        >
-                          {i + 1}
-                        </option>
-                      );
-                    })}
-                    {/* <option selected={'1' == item.quantity ? true : false} value='1'>1</option>
+		let quantity = 0;
+		return (
+			<div>
+				<h2>Cart</h2>
+				{!cart[0] ? (
+					<div>empty cart</div>
+				) : (
+					<div>
+						{cart.map((item, idx) => (
+							<div key={idx} className="item-in-cart">
+								<div>
+									{!products[0] ? (
+										<div>loading</div>
+									) : (
+										<img
+											src={
+												products.filter(
+													(product) => product.id === item.productId
+												)[0].imageUrl
+											}
+											width="150"
+											height="150"
+										/>
+									)}
+								</div>
+								<div>
+									{!products[0] ? (
+										<div>loading</div>
+									) : (
+										products.filter(
+											(product) => product.id === item.productId
+										)[0].name
+									)}
+								</div>
+								<div className="quantity-tracker">
+									in stock:
+									{
+										(quantity = products.filter(
+											(product) => product.id === item.productId
+										)[0].quantity)
+									}
+								</div>
+								<div>
+									Quantity:
+									<select
+										onChange={(event) =>
+											this.changeQty(item, event.target.value)
+										}
+									>
+										{Array.from(Array(quantity), (e, i) => {
+											return (
+												<option
+													value={i + 1}
+													selected={i + 1 == item.quantity ? true : false}
+												>
+													{i + 1}
+												</option>
+											);
+										})}
+										{/* <option selected={'1' == item.quantity ? true : false} value='1'>1</option>
                                         <option selected={'2'== item.quantity ? true : false} value='2'>2</option>
                                         <option selected={'35'== item.quantity ? true : false} value='35'>35</option> */}
-                  </select>
-                  </div>
-                <div className="purchase-price">Price: ${item.purchasePrice / 100}</div>
-                <button
-                  type="button"
-                  onClick={() =>
-                    this.props.deleteItem(
-                      this.props.match.params.userId,
-                      item,
-                      history
-                    )
-                  }
-                >
-                  delete
-                </button>
-              </div>
-            ))}
+									</select>
+								</div>
+								<div className="purchase-price">
+									Price: ${item.purchasePrice / 100}
+								</div>
+								<button
+									type="button"
+									onClick={() =>
+										this.props.deleteItem(
+											this.props.match.params.userId,
+											item,
+											history
+										)
+									}
+								>
+									delete
+								</button>
+							</div>
+						))}
 
-            <button type="button"
-            onClick={() => this.props.checkoutCart(id)}
-            >
-             <a href="/confirmation">Checkout</a>
-            </button>
-          </div>
-        )}
-      </div>
-    );
-  }
+						<button type="button" onClick={() => this.checkout(id)}>
+							<a href="/confirmation">Checkout</a>
+						</button>
+					</div>
+				)}
+			</div>
+		);
+	}
 }
 
 const mapState = (state) => {
@@ -132,11 +145,10 @@ const mapDispatch = (dispatch, { history }) => {
 		getProducts: () => dispatch(fetchProducts()),
 		changeQty: (userId, body, history) =>
 			dispatch(updateQtyThunk(userId, body, history)),
-
 		deleteItem: (userId, body, history) =>
 			dispatch(deleteItemThunk(userId, body, history)),
 		checkoutCart: (userId) => dispatch(checkoutThunk(userId)),
-		addItems: (userId, items) => dispatch(addItemThunk(userId, items))
+		addItems: (userId, items) => dispatch(addItemThunk(userId, items)),
 	};
 };
 
